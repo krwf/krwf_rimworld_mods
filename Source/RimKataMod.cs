7f08fe4e650640c0ce2ec6a42792933ec978813c
KRWF <rimworlduiguide@gmail.com>
2026-09-04 21:34:35 +0900

    Move Harmony bootstrap into RimKataMod.cs

--- Source/RimKataMod.cs
+++ Source/RimKataMod.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using HarmonyLib;
 using RimWorld;
 using UnityEngine;
@@ -8,6 +9,42 @@ using Verse;
 
 namespace KRWF.RimKata
 {
+    [StaticConstructorOnStartup]
+    public static class RimKataBootstrap
+    {
+        static RimKataBootstrap()
+        {
+            Harmony harmony;
+            try
+            {
+                harmony = new Harmony("krwf.rimkata");
+            }
+            catch (Exception exception)
+            {
+                Log.Error("[RimKata] Could not create the Harmony instance.\n" + exception);
+                return;
+            }
+
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception exception)
+            {
+                Log.Error("[RimKata] Core Harmony patching failed; initialization will continue.\n" + exception);
+            }
+
+            try
+            {
+                Patch_Projectile_Impact_Context.Apply(harmony);
+            }
+            catch (Exception exception)
+            {
+                Log.Error("[RimKata] Projectile.Impact patch discovery failed; initialization will continue.\n" + exception);
+            }
+        }
+    }
+
     public sealed class RimKataMod : Mod
     {
         private sealed class RuntimeSettingsSnapshot
